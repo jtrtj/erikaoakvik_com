e8b44e40eace547fe553cad3ce7aae4ea584d73c
John Roemer <jtrtj@jtrtj-computer.lan>
2022-07-24 01:03:53 -0600

    Mark as under construction.

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,17 +5,21 @@ import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+    <div class={tw`p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4`}>
+    <div class={tw`shrink-0`}>
       <img
+        class={tw`h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo`}
         src="/logo.svg"
         height="100px"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
-      <p class={tw`my-6`}>
-      	Erika Oakvik
-	~~under construction~~
-      </p>
-      <Counter start={3} />
+      </div>
+      <div>
+      <div class={tw`text-xl font-medium text-black`}>Erika Oakvik</div>
+      <p class={tw`text-slate-500`}>under construction</p>
+      </div>
     </div>
   );
 }
+// <Counter start={3} />
+
